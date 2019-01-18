e5564297852c7f687070708dd5cff83340011b5e
langhuonan <1921219654@qq.com>
2019-01-18 13:57:34 +0800

    Site updated: 2019-01-18 13:57:33

---
+++ js/src/love.js
@@ -0,0 +1,26 @@
+.mod - contentscript# formattingMsg {
+    position: absolute;top: 0;font - size: 14 px;color: #333;margin:5px;}# formattingMsg.x - loading {
+        width: 12 px;height: 12 px;border: 1 px solid# f00;border - radius: 50 % ;box - shadow: 0 0 10 px 2 px;color: #c00;border -
+        right - color: transparent;border - top - color: transparent;animation: spin - right 1 s linear infinite normal;animation -
+        delay: 0 s;margin: 0 5 px 0 0;display: inline - block
+    }#
+    formattingMsg.x - loading: before {
+        display: block;width: 8 px;height: 8 px;margin: 1 px;border: 2 px solid# f00;content: " ";border -
+        radius: 50 % ;border - left - color: transparent;border - bottom - color: transparent
+    }
+    @keyframes spin - right {
+        from {
+            transform: rotate(0 deg);opacity: .2
+        }
+        50 % {
+            transform: rotate(180 deg);opacity: 1.0
+        }
+        to {
+            transform: rotate(360 deg);opacity: .2
+        }
+    }
+    格式化中...{
+        "error": "no such domain"
+    } {
+        "error": "no such domain"
+    }
